86c9e031e1bcc18c93f0fcca741a02e8f1c1fcb5
Puneet Sachdev <psachdev@lap-tz-0x004843.local>
2018-03-26 20:44:37 +0530

    Updated zuul start command

--- start-infrastructure.sh
+++ start-infrastructure.sh
@@ -3,5 +3,5 @@ echo 'Started Configserver in Docker Container'
 docker run -p 8010:8010 --name eureka -d com.puneet/eurekaserver
 echo 'Started Eureka server in Docker Container'
 sleep 10
-docker run --name zuul  --link config:config --link eureka:eureka -itd com.puneet/zuulserver
+docker run --name zuul  -p 8080:8080 --link config:config --link eureka:eureka -itd com.puneet/zuulserver
 echo 'Started Zuul Proxy in Docker Container'
