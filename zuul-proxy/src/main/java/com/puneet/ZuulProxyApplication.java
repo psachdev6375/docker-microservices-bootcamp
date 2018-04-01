c3dea70907543c9889188a43244d05223714e55e
Puneet Sachdev <psachdev@lap-tz-0x004843.lan>
2018-04-01 22:50:10 +0300

    Added Zuul to Prometheus monitoring

--- zuul-proxy/src/main/java/com/puneet/ZuulProxyApplication.java
+++ zuul-proxy/src/main/java/com/puneet/ZuulProxyApplication.java
@@ -7,11 +7,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+import io.prometheus.client.spring.boot.EnablePrometheusEndpoint;
+import io.prometheus.client.spring.boot.EnableSpringBootMetricsCollector;
+
 /**
  * The Class ZuulProxyApplication.
  */
 @SpringBootApplication
 @EnableZuulProxy
+@EnablePrometheusEndpoint
+@EnableSpringBootMetricsCollector
 public class ZuulProxyApplication {
 
     /**
